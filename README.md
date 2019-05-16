5513ec43b6f77b0abf99327d378e3743dc25f84c
Henry Park <henrypark@Henrys-MBP.attlocal.net>
2019-05-15 21:06:03 -0500

    AWS keys have been taken down

--- README.md
+++ README.md
@@ -15,7 +15,13 @@ conda activate hpark<br>
 
 ## Running the python script
 
-1) Run data_download.py python script to download the h1b.csv from AWS S3
-2) Run data_upload.py python script to upload the h1b.csv from current directory to AWS S3
+1) Run data_download.py python script to download the h1b.csv from AWS S3<br>
+data_download.py takes multiple args where the first two args must be inputted where
+first corresponds to aws_access_key_id and second corresponds to aws_secret_access_key
+<br><br>
+2) Run data_upload.py python script to upload the h1b.csv from current directory to AWS S3<br>
+data_download.py takes multiple args where the first two args must be inputted where
+first corresponds to aws_access_key_id and second corresponds to aws_secret_access_key
+<br><br>
 3) Run models.py python script to create a table in the RDS server
     (A sample row has already been created so you can uncomment the last few lines to see a sample output from a select all query)
--- data_upload.py
+++ data_upload.py
@@ -3,13 +3,15 @@ import argparse
 
 
 def main(args):
-    s3 = boto3.resource(args.aws_resource, aws_access_key_id='AKIAJ7IO2WG3EEVXNHUA', aws_secret_access_key='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')    
+    s3 = boto3.resource(args.aws_resource, aws_access_key_id=args.aws_access_id, aws_secret_access_key=args.aws_secret_access_key)
     bucket = s3.Bucket(args.bucket_name)
     bucket.upload_file(args.filepath, args.outputname)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--aws_access_id', type=str, help='aws_access_id')
+    parser.add_argument('--aws_secret_access_key', type=str, help='aws_secret_access_key')
     parser.add_argument('--aws_resource', type=str, default='s3' , help='AWS data resource (default=s3)')
     parser.add_argument('--bucket_name', type=str, default='nw-henry-s3' , help='Resouce bucket name (default=nw-henry-s3)')
     parser.add_argument('--filepath', type=str, default='./h1b.csv', help='The location of the file in the local (default=./h1b.csv)')
